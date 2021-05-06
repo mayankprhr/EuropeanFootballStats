2bb13f5c372f8f70dac5d4e4a3f08788fcddac76
Mayank Parihar <itsmayankprhr@gmail.com>
2021-05-06 21:41:47 +0530

    Pushing the initial Commit

---
+++ PremierLeagueDashboard/epl-dashboard-master/src/main/java/io/hauxi/epldashboard/EplDashboardApplication.java
@@ -0,0 +1,13 @@
+package io.hauxi.epldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class epldashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(epldashboardApplication.class, args);
+	}
+
+}
